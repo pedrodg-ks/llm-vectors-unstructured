d659e23ce3dfce3403cd4261a4d549a61ecfd17c
pedrodg-ks <pedro.goncalves@thekeenfolks.com>
2025-03-18 12:20:53 +0100

    added script to build the module graph in neo4j

--- llm-vectors-unstructured/build_graph.py
+++ llm-vectors-unstructured/build_graph.py
@@ -22,14 +22,66 @@ chunks = text_splitter.split_documents(docs)
 
 # Create a function to get the embedding
 
+def get_embedding(llm, text):
+    response = llm.embeddings.create(
+            input=text,
+            model="text-embedding-ada-002"
+        )
+    return response.data[0].embedding
+
 # Create a function to get the course data
+
+def get_course_data(llm, chunk):
+    data = {}
+
+    path = chunk.metadata['source'].split(os.path.sep)
+
+    data['course'] = path[-6]
+    data['module'] = path[-4]
+    data['lesson'] = path[-2]
+    data['url'] = f"https://graphacademy.neo4j.com/courses/{data['course']}/{data['module']}/{data['lesson']}"
+    data['text'] = chunk.page_content
+    data['embedding'] = get_embedding(llm, data['text'])
+
+    return data
 
 # Create OpenAI object
 
+llm = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
 # Connect to Neo4j
+
+driver = GraphDatabase.driver(
+    os.getenv('NEO4J_URI'),
+    auth=(
+        os.getenv('NEO4J_USERNAME'),
+        os.getenv('NEO4J_PASSWORD')
+    )
+)
+
+driver.verify_connectivity()
 
 # Create a function to run the Cypher query
 
+def create_chunk(tx, data):
+    tx.run("""
+        MERGE (c:Course {name: $course})
+        MERGE (c)-[:HAS_MODULE]->(m:Module{name: $module})
+        MERGE (m)-[:HAS_LESSON]->(l:Lesson{name: $lesson, url: $url})
+        MERGE (l)-[:CONTAINS]->(p:Paragraph{text: $text})
+        WITH p
+        CALL db.create.setNodeVectorProperty(p, "embedding", $embedding)
+        """, 
+        data
+        )
+
 # Iterate through the chunks and create the graph
+for chunk in chunks:
+    with driver.session(database="neo4j") as session:
+        session.execute_write(
+            create_chunk,
+            get_course_data(llm, chunk)
+        )
 
 # Close the neo4j driver
+driver.close()
